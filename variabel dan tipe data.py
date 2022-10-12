b72bf52b7fdd683bfe275baafb7250d253640a9b
burgerkill098 <115003776+burgerkill098@users.noreply.github.com>
2022-10-12 16:40:51 +0700

    Add files via upload

---
+++ variabel dan tipe data.py
@@ -0,0 +1,23 @@
+price= 100000
+
+print (price)
+
+
+discount=0.5
+print(discount)
+
+price= price*discount
+print(price)
+
+name= "gundam"
+print(name)
+
+is_discount= True
+print(is_discount)
+
+is_discount= False
+print(is_discount)
+
+
+
+
